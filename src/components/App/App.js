438d934a9f0971e5442d52fced307341c637d804
luke rohde <morneau33@gmail.com>
2020-02-06 10:41:27 -0600

    studying routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,6 +16,7 @@ import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
 import AboutPage from '../AboutPage/AboutPage';
 import UserPage from '../UserPage/UserPage';
 import SecretsPage from '../SecretsPage/SecretsPage';
+import Boom from '../Boom/Boom';
 
 import './App.css';
 
@@ -55,6 +56,11 @@ class App extends Component {
               path="/secrets"
               component={SecretsPage}
             />
+            <ProtectedRoute
+              exact
+              path = "/boom"
+              component={Boom}
+              />
             {/* If none of the other routes matched, we will show a 404. */}
             <Route render={() => <h1>404</h1>} />
           </Switch>
---
+++ src/components/Boom/Boom.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Boom extends Component {
+
+    render() {
+        return (
+            <div>
+                boom
+            </div>
+        )
+    }
+}
+
+export default Boom;
